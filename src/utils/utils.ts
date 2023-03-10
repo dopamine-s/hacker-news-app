a196b6c0cedf19b828104249960044b06bb64a2a
dopamine-s <motor55005@gmail.com>
2023-03-10 18:21:27 +0100

    handling errors with date format

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,13 @@
 import { formatDistanceToNow } from 'date-fns';
 
 export function getTimeAgo(date: number): string {
-  const dateObj = new Date(date * 1000); // convert Unix timestamp to Date object
-  return formatDistanceToNow(dateObj, { addSuffix: true });
+  try {
+    const dateObj = new Date(date * 1000); // convert Unix timestamp to Date object
+    return formatDistanceToNow(dateObj, { addSuffix: true });
+  } catch (error) {
+    console.error('Error converting Unix timestamp to Date object:', error);
+    return 'Invalid date';
+  }
 }
 
 export function getDomainFromLink(link: string): string {
